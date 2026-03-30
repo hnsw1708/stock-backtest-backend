67515c2b8992095355edcce3f3d186cc7c735e37
anupam1708 <anupam.1708@gmail.com>
2026-03-29 21:55:31 -0500

    fix: Upgrade Java 11 → 21 (LTS): applied OpenRewrite recipes (org.openrewrite.java.migrate.UpgradeToJava2

--- src/main/java/com/example/stockbacktest/controller/ChartDataController.java
+++ src/main/java/com/example/stockbacktest/controller/ChartDataController.java
@@ -7,6 +7,8 @@ import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
 @RestController
 @RequestMapping("/api")
 public class ChartDataController {
